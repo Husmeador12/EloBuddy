ef33ae5a98bc27f0f5f6689e6cfd2b9518af2bae
Husmeador12 <asus12@gmx.com>
2015-09-29 12:17:49 +0200

    Creating IPBuddy

---
+++ IPBuddy/IPBuddy/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Threading;
+using System.Net;
+
+namespace IPBuddy
+{
+    class Program
+    {
+        public static Menu IPBuddyMenu;
+
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Game_OnStart;
+        }
+
+        private static void Game_OnStart(EventArgs args)
+        {
+                ///IPBuddyMenu.Add(new MenuItem("IPwinn", "At Win: "));
+               /// IPBuddyMenu.Add(new MenuItem("IPlosee", "At Lose: "));
+                ///IPBuddyMenu.Add(new MenuItem("ipminute", ""));
+               /// var press1 = IPBuddyMenu.Add(new MenuItem("tiklagelsin", "Write Key").SetValue(new KeyBind(85, KeyBindType.Press)));
+               /// IPBuddyMenu.Add(new MenuItem("warnmessge", "Only you can see chat texts."));
+
+            Chat.Print("<font color = \"#0099FF\">IPBuddy<font color = \"#FFFFCC\"> is loaded. ^.^</font>");
+            IPBuddyMenu = MainMenu.AddMenu("IPBuddy", "IPBuddy");
+            IPBuddyMenu.AddGroupLabel("IPBuddyMenu");
+            IPBuddyMenu.AddLabel("Credits to Kindred.");
+            IPBuddyMenu.Add("enabled", new CheckBox("See IP buddy", false));
+
+            Game.OnUpdate += OnUpdate;
+        }
+
+        public static void OnUpdate(EventArgs args)
+        {
+
+            double ipminute = Game.Time / 60 - 1;
+            float gametimer = Game.Time / 60 - 1;
+            double winnip = 18 + 2.312 * ipminute;
+            double loseeip = 16 + 1.405 * ipminute;
+            ///IPBuddyMenu.AddLabel("At win you will get " + winnip + " IP!");
+            ///IPBuddyMenu.AddLabel("At lose you will get " + loseeip + " IP!");
+            ///IPBuddyMenu.AddLabel("Minute is " + gametimer);
+
+            double tick = 0;
+            tick = TimeSpan.FromSeconds(Environment.TickCount).Minutes;
+            if (IPBuddyMenu["enabled"].Cast<CheckBox>().CurrentValue && tick == 30)
+            {
+                WriteIPBuddy();
+            }
+        }
+        public static void WriteIPBuddy()
+        {
+            double ipminute = Game.Time / 60 - 1;
+            double winnip = 18 + 2.312 * ipminute;
+            double loseeip = 16 + 1.405 * ipminute;
+            Chat.Print("<font color = \"#39f613\">At win you will get </font>" + winnip + " IP!");
+            Chat.Print("<font color = \"#FF0000\">At lose you will get </font>" + loseeip + " IP!");
+        }
+    }
+}
--- IPBuddy/IPBuddy/Properties/AssemblyInfo.cs
+++ IPBuddy/IPBuddy/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general sobre un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos atributos para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("JARAM")]
-[assembly: AssemblyDescription("Bot for ARAM")]
+[assembly: AssemblyTitle("IPBuddy")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("JARAM")]
+[assembly: AssemblyCompany("Husmeador12")]
+[assembly: AssemblyProduct("IPBuddy")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
-[assembly: Guid("060b5f4d-b6c5-4369-bb6f-7322dd6d8c19")]
+[assembly: Guid("f4671168-4887-4b8d-9036-0671445d5dbd")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
